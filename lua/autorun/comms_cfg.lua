95fab9b180c78fe7050da06f933a6de04f5a3321
RCTGames <rctgamestv@gmail.com>
2017-11-05 10:04:39 +0100

    Changed comms color and added commsOOC color

--- lua/autorun/comms_cfg.lua
+++ lua/autorun/comms_cfg.lua
@@ -2,7 +2,12 @@ if SERVER then
 	cfgComms = {}
 	cfgComms.ooc = true
 	cfgComms.comms = true
-	cfgComms.color = {
+	cfgComms.colorComms = {
+		"255", --Red
+		"255", --Green
+		"255" --Blue
+	}
+	cfgComms.colorOOC = {
 		"255", --Red
 		"255", --Green
 		"255" --Blue
